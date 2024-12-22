abe09ba5da84d85afead48019430704c7232e7c8
mikekellydev <mikekellydev@gmail.com>
2024-12-21 23:15:38 -0500

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Grassroots Radio Advocates Website Loaded");
 });
 
+{% if page.categories %}
+  <p>Categories: 
+    {% for category in page.categories %}
+      <a href="/categories/{{ category | slugify }}">{{ category }}</a>{% if forloop.last == 
+false %}, {% endif %}
+    {% endfor %}
+  </p>
+{% endif %}
