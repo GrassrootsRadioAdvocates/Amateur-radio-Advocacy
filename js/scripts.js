eea86a6b619f333bc5d3db4b92536f6ed491f005
mikekellydev <mikekellydev@gmail.com>
2024-12-22 09:32:33 -0500

    adding blog section to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
-// Basic JavaScript functionality
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Grassroots Radio Advocates Website Loaded");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  // Sample blog posts data
+  const blogPosts = [
+    {
+      title: "First Blog Post",
+      summary: "This is a summary of the first blog post.",
+      link: "blog/post1.html",
+    },
+    {
+      title: "Second Blog Post",
+      summary: "This is a summary of the second blog post.",
+      link: "blog/post2.html",
+    },
+    {
+      title: "Third Blog Post",
+      summary: "This is a summary of the third blog post.",
+      link: "blog/post3.html",
+    },
+  ];
 
-{% if page.categories %}
-  <p>Categories: 
-    {% for category in page.categories %}
-      <a href="/categories/{{ category | slugify }}">{{ category }}</a>{% if forloop.last == 
-false %}, {% endif %}
-    {% endfor %}
-  </p>
-{% endif %}
+  // Get the container
+  const blogPostsContainer = document.getElementById("blog-posts");
+
+  // Add the last three blog posts
+  blogPosts.slice(0, 3).forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("blog-post");
+    postElement.innerHTML = `
+      <h3><a href="${post.link}">${post.title}</a></h3>
+      <p>${post.summary}</p>
+    `;
+    blogPostsContainer.appendChild(postElement);
+  });
+});
